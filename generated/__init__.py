ea44d197e7eca9be64876463bebf729fcc98b7b6
ryleu <69326171+ryleu@users.noreply.github.com>
2025-01-21 05:42:01 +0000

    add new proto files for pings and core_feedback

--- generated/__init__.py
+++ generated/__init__.py
@@ -5,5 +5,6 @@ To generate the classes, run `poetry run build`.
 """
 
 from generated.controller_pb2 import Controller as ProtoController
-from generated.vector_pb2 import Vector as ProtoVector
-from generated.any_pb2 import Any as ProtoAny
+from generated.core_feedback_pb2 import CoreFeedback as ProtoCoreFeedback
+from generated.vector_pb2 import Vector as ProtoVector, Vector3 as ProtoVector3
+from generated.ping_pb2 import Ping as ProtoPing
