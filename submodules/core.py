db67ba72c2378376c60d0c4310160a358a080abe
Broken-Admin <alexlresu@gmail.com>
2025-01-31 06:38:46 +0000

    Updated core.py +/- Implemented websocket handling into core submodule

--- submodules/core.py
+++ submodules/core.py
@@ -1,9 +1,16 @@
 from submodules import Submodule
-from interfaces_pkg.msg import ControllerState, CoreFeedback
+
+# ControllerState & CoreFeedback
+import interfaces_pkg.msg
 from util.ros_utils import convert_controller
 from util.aiohttp_utils import WSSender
 from rclpy.node import Node
 import logging
+
+LOG = logging.getLogger(__name__)
+
+# Websocket data handling
+from util import websocket_types
 
 
 class Core(Submodule):
@@ -17,20 +24,41 @@ class Core(Submodule):
         super().__init__(node, "core", ws_sender)
 
         self._controller_publisher = self._node.create_publisher(
-            ControllerState, "/astra/core/controller", 10
+            interfaces_pkg.msg.ControllerState,
+            "/astra/core/controller",
+            10,
         )
         self._feedback_subscriber = self._node.create_subscription(
-            CoreFeedback, "/astra/core/telemetry", self._feedback_callback, 10
+            interfaces_pkg.msg.CoreFeedback,
+            "/astra/core/telemetry",
+            self._feedback_callback,
+            10,
         )
 
         for ws_msg in self._ws_map.keys():
             self.LOG.debug(f"listening for {ws_msg}")
 
-    def _handle_controller_msg(self, ws_msg):
-        if ws_msg.submodule == "core":
-            self._controller_publisher.publish(convert_controller(ws_msg))
+    def handle_ws_msg(self, ws_data: websocket_types.WebSocketData) -> bool:
+        # Only handle controller data
+        if (
+            type(ws_data) != websocket_types.ControllerData
+            or ws_data.loaded_data["submodule"] == ""
+        ):
+            return False
 
-    def _feedback_callback(self, msg: CoreFeedback):
+        # Handle the controller data
+        self._handle_controller_msg(ws_data)
+
+    # Process data handling from a websocket and publish it
+    def _handle_controller_msg(
+        self, ws_controller: websocket_types.ControllerData
+    ) -> None:
+        # Process controller data
+        processed_controller_data = self._proc_controller(ws_controller)
+        # Publish to the ROS2 topic
+        self._controller_publisher.publish(processed_controller_data)
+
+    def _feedback_callback(self, msg: interfaces_pkg.msg.CoreFeedback) -> None:
         """
         Convert CoreFeedback message to ProtoCoreFeedback and send it to the websockets.
         """
@@ -50,3 +78,36 @@ class Core(Submodule):
         ws_msg.bmp_pressure = msg.bmppres
 
         self._ws_sender.send(ws_msg.SerializeToString())
+
+    # Process websocket data into a ROS2 message
+    def _proc_controller(
+        self,
+        ws_controller: websocket_types.ControllerData,
+    ) -> interfaces_pkg.msg.ControllerState:
+        LOG.debug("Processing loaded data into interfaces_pkg.msg.ControllerState")
+        return interfaces_pkg.msg.ControllerState(
+            # triggers and bumpers
+            lt=ws_controller.loaded_data["left_trigger"],
+            rt=ws_controller.loaded_data["right_trigger"],
+            lb=ws_controller.loaded_data["l_bumper"],
+            rb=ws_controller.loaded_data["r_bumper"],
+            # plus and minus
+            plus=ws_controller.loaded_data["option"],
+            minus=ws_controller.loaded_data["share"],
+            # left stick
+            ls_x=ws_controller.loaded_data["la_vector"].x,
+            ls_y=ws_controller.loaded_data["la_vector"].y,
+            # right stick
+            rs_x=ws_controller.loaded_data["ra_vector"].x,
+            rs_y=ws_controller.loaded_data["ra_vector"].y,
+            # face buttons
+            a=ws_controller.loaded_data["a"],
+            b=ws_controller.loaded_data["b"],
+            x=ws_controller.loaded_data["x"],
+            y=ws_controller.loaded_data["y"],
+            # dpad
+            d_up=ws_controller.loaded_data["up"],
+            d_down=ws_controller.loaded_data["down"],
+            d_left=ws_controller.loaded_data["left"],
+            d_right=ws_controller.loaded_data["right"],
+        )
