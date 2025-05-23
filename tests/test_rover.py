c3191f23ae510b3dfcad1b8f4e49fd687d575f40
ryleu <69326171+ryleu@users.noreply.github.com>
2025-05-23 04:53:45 +0000

    update tests to use more realistic data

--- tests/test_rover.py
+++ tests/test_rover.py
@@ -6,6 +6,7 @@ from ros2_interfaces_pkg import msg
 from enum import Enum
 import argparse
 from asyncio import run
+import random
 
 """
 This script generates random ROS2 messages and publishes them to a ROS2 topic.
@@ -23,6 +24,10 @@ class CoreNode(Node):
         to_send = generate_cumulative_data(
             websocket_types.CoreFeedbackData.from_ros(msg.CoreFeedback())
         )
+        to_send.data["gps_lat"] = random.uniform(38, 39)
+        to_send.data["gps_long"] = random.uniform(-110, -111)
+        to_send.data["orientation"] = random.uniform(-180, 180)
+
         self.publisher_.publish(to_send.to_ros())
         self.get_logger().info('Publishing: "%s"' % to_send.data)
 
@@ -38,6 +43,11 @@ class CoreAutoNode(Node):
         to_send = generate_random_data(
             websocket_types.AutoFeedbackData.from_ros(msg.AutoFeedback())
         )
+        # get a random coord near where the mars desert research station is
+        to_send.data["gps_lat"] = random.uniform(38, 39)
+        to_send.data["gps_long"] = random.uniform(-110, -111)
+        to_send.data["orientation"] = random.uniform(-180, 180)
+
         self.publisher_.publish(to_send.to_ros())
         self.get_logger().info('Publishing: "%s"' % to_send.data)
 
