2bcc4caf9b0e4fdf1f77fd16f181616e2c553625
ryleu <69326171+ryleu@users.noreply.github.com>
2025-05-25 01:18:40 +0000

    the purge.

--- tests/test_rover.py
+++ tests/test_rover.py
@@ -52,18 +52,16 @@ class CoreAutoNode(Node):
         self.get_logger().info('Publishing: "%s"' % to_send.data)
 
 
-class FaerieNode(Node):
+class BioNode(Node):
     def __init__(self):
         super().__init__("test_publisher")
-        self.publisher_ = self.create_publisher(
-            msg.FaerieFeedback, "/arm/feedback/faerie", 10
-        )
+        self.publisher_ = self.create_publisher(msg.BioFeedback, "/bio/feedback", 10)
         timer_period = 1.0  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
         to_send = generate_cumulative_data(
-            websocket_types.BioFeedbackData.from_ros(msg.FaerieFeedback()),
+            websocket_types.BioFeedbackData.from_ros(msg.BioFeedback()),
             (0, 1),
             (0, 255),
         )
@@ -102,7 +100,7 @@ class SocketNode(Node):
 class NodeEnum(Enum):
     core = CoreNode
     auto = CoreAutoNode
-    faerie = FaerieNode
+    bio = BioNode
     socket = SocketNode
 
     def __str__(self):
